d9c3502368d6a9ab25805117dcdc85797f548f1e
aleempasha2772 <aleempasha1410@gmail.com>
2025-01-21 22:57:50 +0530

    Star Pyramid left

--- src/main/java/com/example/java/Patterns/StarPyramid.java
+++ src/main/java/com/example/java/Patterns/StarPyramid.java
@@ -5,7 +5,7 @@ public class StarPyramid {
         int n = 5;
         for(int i=0;i<=n;i++){
             for(int j=0;j<=i;j++){
-                System.out.print(" * ");
+                System.out.print(" *");
             }
             System.out.println();
         }
