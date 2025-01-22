940a2b80bbff7a7742e5437aefb34593795377cf
aleempasha2772 <aleempasha1410@gmail.com>
2025-01-22 17:02:43 +0530

    Pyramid

--- src/main/java/com/example/java/Patterns/StarPyramid.java
+++ src/main/java/com/example/java/Patterns/StarPyramid.java
@@ -3,6 +3,21 @@ package com.example.java.Patterns;
 public class StarPyramid {
     public static void main(String[] args) {
         int n = 5;
+        int space = n-1;
+        for(int i=0;i<n;i++){
+            for(int j=0; j<space;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+            space--;
+        }
+
+
+
+
         for(int i=0;i<=n;i++){
             for(int j=0;j<=i;j++){
                 System.out.print(" *");
