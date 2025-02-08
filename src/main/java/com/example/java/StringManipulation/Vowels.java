3829cc595bbb4b732ec0070bca640f62bb1fa6c0
aleempasha2772 <aleempasha1410@gmail.com>
2025-02-08 13:05:15 +0530

    Vowels and Consonents Count

---
+++ src/main/java/com/example/java/StringManipulation/Vowels.java
@@ -0,0 +1,20 @@
+package com.example.java.StringManipulation;
+
+public class Vowels {
+	public static void main(String[] args) {
+		String s = "Aleem";
+		String s1 = s.toLowerCase();
+		char[] a = s1.toCharArray();
+		int v=0,c=0;
+		for(int i=0;i<s.length();i++) {
+			if(a[i]=='a'|| a[i] =='e'|| a[i] == 'i'|| a[i]=='o'||a[i]=='u') {
+				v++;
+			}else {
+				c++;			}
+		}
+		System.out.println(v);
+		System.out.println(c);
+		
+		
+	}
+}
