ec716a24c4902f22dde1c32855d75d77a3e4ce7a
aleempasha2772 <aleempasha1410@gmail.com>
2026-01-23 12:10:51 +0530

    Bubble sort

---
+++ src/main/java/com/example/java/Sorting/BubbleSort.java
@@ -0,0 +1,39 @@
+package com.example.java.Sorting;
+
+public class BubbleSort {
+	
+	public static int[] sort(int[] arr) {
+		int n = arr.length;
+		for(int i=0;i<n;i++) {
+			for(int j = i+1;j<n;j++) {
+				if(arr[i]>arr[j]) {
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+		
+		return arr;
+	}
+	
+	public static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+	public static void main(String[] args) {
+		int[] arr = {2,5,9,3,7,4,11,13};
+		
+		System.out.println("before sorting");
+		printArray(arr);
+		System.out.println("After sorting");
+		sort(arr);
+		printArray(arr);
+		
+	}
+
+}
